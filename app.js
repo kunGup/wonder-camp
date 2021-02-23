58e9f7c35feded5ddcb3e3f52b98e024deb2e890
Kunal Gupta <goodkunal723@gmail.com>
2021-02-23 15:17:45 +0530

    fixing security issues & deploying

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-if(process.env.NODE_ENV !== "production"){
-    require('dotenv').config()
-}
+// if(process.env.NODE_ENV !== "production"){
+//     require('dotenv').config()
+// }
 
+// 
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const flash = require('connect-flash')
@@ -12,11 +14,15 @@ const ejsMate = require('ejs-mate')
 const campgroundsRoute = require('./routes/campgrounds')
 const reviewsRoute = require('./routes/reviews')
 const usersRoute = require('./routes/users')
-
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const MongoStore = require('connect-mongo')(session)
 const User = require('./models/user');
 const LocalStrategy = require('passport-local')
 const passport = require('passport')
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+
+const dbUrl = process.env.ExpressErrorDB_URL || 'mongodb://localhost:27017/yelp-camp'
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
@@ -37,8 +43,21 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize({
+    replaceWith: '_'
+  }));
+app.use(helmet())
 
+const store = new MongoStore({
+    url: dbUrl,
+    secret: 'thisshouldbeabettersecret',
+    touchAfter: 24*60*60
+})
+store.on("error",function(e){
+    console.log("SESSION STORE ERROR",e);
+})
 const sessionConfig = {
+    store,
     secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
@@ -51,6 +70,50 @@ const sessionConfig = {
 
 app.use(session(sessionConfig))
 app.use(flash())
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/kungup723/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -81,6 +144,8 @@ app.use((err, req, res, next) => {
     res.status(status)
     res.render('error', { err })
 })
-app.listen('3000', () => {
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
     console.log("CONNECTION OPEN AT 3000");
 })
--- seeds/index.js
+++ seeds/index.js
@@ -34,12 +34,12 @@ const seedDB = async ()=>{
             },
             images: [
                 {
-                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
-                    filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
+                    url: 'https://res.cloudinary.com/kungup723/image/upload/v1613831068/Wonder-camp/an5srawsifspck9yzcog.jpg',
+                    filename: 'Wonder-camp/an5srawsifspck9yzcog'
                 },
                 {
-                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
-                    filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
+                    url: 'https://res.cloudinary.com/kungup723/image/upload/v1613830219/Wonder-camp/xsmhyiuoyojjffnbr4hb.jpg',
+                    filename: 'Wonder-camp/xsmhyiuoyojjffnbr4hb'
                 }
             ]
         })
